3537b9eccbef8bdac5c7bffbb3eab1ac79627049
Anderson Batista <Anderson Batista@DELL-ANDERSON>
2016-10-20 00:28:32 -0400

    v.1.0.19 - 20/10/2016 - Incluido formulario para a pagina de Contato.

--- themes/fonsecaeassis/contato.php
+++ themes/fonsecaeassis/contato.php
@@ -8,11 +8,34 @@
                     <li class="active">Contato</li>
                 </ol>
                 <div class="contato-dados">
-                    <h5>Atendimento</h5>
+                    <h5><b>Canais de Atendimento</b></h5>
                     <h6><b>Telefone:</b> (69) 3224-6357</h6>
                     <h6><b>E-mail:</b> contato@exemplo.com.br</h6>
-                    <h5>Localização</h5>
+                    <h5><b>Localização</b></h5>
                     <h6>Rua Joaquim Nabuco, 1774 - Porto Velho-RO</h6>
+                </div>
+                <div class="contato-form">
+                    <form>
+                        <div class="form-group">
+                            <label  class="titulo-label">Nome</label>
+                            <input type="text" class="nome form-control" placeholder="Informe seu nome">
+                        </div>
+                        <div class="form-group">
+                            <label class="titulo-label">E-mail</label>
+                            <input type="email" class="email form-control" placeholder="Informe seu e-mail">
+                        </div>
+                        <div class="form-group">
+                            <label class="titulo-label">Assunto</label>
+                            <input type="text" class="assunto form-control" placeholder="Informe o assunto">
+                        </div>
+                        <div class="form-group">
+                            <label class="titulo-label">Mensagem</label>
+                            <textarea class="mensagem form-control" rows="8" placeholder="Digite sua mensagem"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-enviar">Enviar</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
