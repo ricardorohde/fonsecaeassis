826d319fb6d01e79ee9cc6c612375ea1b7e8db7a
Anderson Batista <Anderson Batista@DELL-ANDERSON>
2016-10-20 00:51:34 -0400

    v.1.0.20 - 20/10/2016 - Ajuste de layout de leitura de noticias, artigos e publicações.

--- themes/fonsecaeassis/noticia.php
+++ themes/fonsecaeassis/noticia.php
@@ -6,13 +6,15 @@ else:
     header('Location: ' . HOME);
 endif;
 ?>
-<div class="content">
-    <div class="main_left">
-        <div class="main_content">
-            <header class="header_content">
-                <h1 class="header_title"><?= $titulo; ?></h1>
-                <p class="header_subtitle"><?= $subtitulo; ?></p>
-                <span class="header_line"></span>
+<div class="box-conteudo-paginas">    
+    <div class="container">
+        <div class="row">
+            <div class="box-100">
+                <h5 class="titulo-paginas"><?= $titulo; ?></h5>
+                <ol class="breadcrumb">
+                    <li><a href="index.php">Home</a></li>
+                    <li class="active"><?= $subtitulo; ?></li>
+                </ol>
                 <div class="header_social">
                     <a href="https://www.facebook.com/sharer/sharer.php?u=<?= HOME . '/noticia/' . $url_name; ?>" title="Compartilhar no Facebook" class="rds_facebook" target="_blank"><i class="fa fa-facebook-square fa-2x"></i></a>
                     <a href="https://twitter.com/home?status=<?= HOME . '/noticia/' . $url_name; ?>" title="Compartilhar no Twitter" class="rds_twitter" target="_blank"><i class="fa fa-twitter-square fa-2x"></i></a>
@@ -25,83 +27,86 @@ endif;
                     <?php if (!empty($fonte)) : ?><div class="fonte"><b>Fonte:</b> <?= $fonte; ?></div><?php endif; ?>
                     <?php if (!empty($autor)) : ?><div class="autor"><b>Autor:</b> <?= $autor; ?></div><?php endif; ?>
                 </div>
-            </header>
-            <article class="content_pag">
-                <div class="article_news_chars"><?= $noticia; ?></div>
-                <?php if (!empty($foto)): ?>
-                    <figure class="article_news_image">
-                        <a href="<?= HOME . '/uploads/' . $foto; ?>">
-                            <img alt="<?= $titulo; ?>" title="<?= $titulo; ?>" src="<?= HOME . '/tim.php?src=uploads/' . $foto . '&w=870&zc=0'; ?>"/>
-                        </a>
-                    </figure>
-                    <?php
-                endif;
+                <article class="content_pag">
+                    <div class="article_news_chars"><?= $noticia; ?></div>
+                    <?php if (!empty($foto)): ?>
+                        <figure class="article_news_image">
+                            <a href="<?= HOME . '/uploads/' . $foto; ?>">
+                                <img alt="<?= $titulo; ?>" title="<?= $titulo; ?>" src="<?= HOME . '/tim.php?src=uploads/' . $foto . '&w=870&zc=0'; ?>"/>
+                            </a>
+                        </figure>
+                        <?php
+                    endif;
 
-                //Video
-                if (!empty($video)):
-                    ?>
-                    <div class="article_news_morenews">
-                        <header class="header_morenews">
-                            <h1 class="header_morenews_vin">Veja o vídeo abaixo:</h1>
-                        </header>
-                        <div class="content_morenews">
-                            <div class="frameBox">
-                                <div class="ratio16">
-                                    <iframe class="ratio_element"  width="100%" height="100%" src="https://www.youtube.com/embed/<?= Check::ytVideo($video); ?>" frameborder="0" allowfullscreen></iframe>
+                    //Video
+                    if (!empty($video)):
+                        ?>
+                        <div class="article_news_morenews">
+                            <header class="header_morenews">
+                                <h1 class="header_morenews_vin">Veja o vídeo abaixo:</h1>
+                            </header>
+                            <div class="content_morenews">
+                                <div class="frameBox">
+                                    <div class="ratio16">
+                                        <iframe class="ratio_element"  width="100%" height="100%" src="https://www.youtube.com/embed/<?= Check::ytVideo($video); ?>" frameborder="0" allowfullscreen></iframe>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <?php
-                endif;
-                //Outras Fotos
-                $OutrasFotos = new Read;
-                $OutrasFotos->ExeRead("banco_fotos", "WHERE id_tipo = :idtipo AND tipo = :tipo ORDER BY ordem ASC", "idtipo={$id}&tipo=N");
-                if ($OutrasFotos->getResult()):
-                    ?>
-                    <div class="article_news_moreimg">
                         <?php
-                        foreach ($OutrasFotos->getResult() as $fotos):
-                            echo '<div class="boxfotos">';
-                            echo '<img alt="' . $titulo . '" title="' . $titulo . '" src="' . HOME . '/tim.php?src=uploads/' . $fotos['foto'] . '&w=870"/>';
-                            echo '<p>' . $fotos['legenda'] . '</p>';
-                            echo '</div>';
-                        endforeach;
+                    endif;
+                    //Outras Fotos
+                    $OutrasFotos = new Read;
+                    $OutrasFotos->ExeRead("banco_fotos", "WHERE id_tipo = :idtipo AND tipo = :tipo ORDER BY ordem ASC", "idtipo={$id}&tipo=N");
+                    if ($OutrasFotos->getResult()):
                         ?>
-                    </div>
-                    <?php
-                endif;
-                ?>
-                <div class="article_news_morenews">
-                    <header class="header_morenews">
-                        <h1 class="header_morenews_vin">Outras Notícias</h1>
-                    </header>
-                    <div class="content_morenews">
-                        <?php
-                        $ReadMoreNews = new Read;
-                        $ReadMoreNews->ExeRead('noticias', "WHERE titulo != :titulo AND categoria = :categoria AND id != :id AND foto != :foto ORDER BY id DESC LIMIT :limit", "titulo=''&categoria={$categoria}&id={$id}&foto=''&limit=4");
-                        if ($ReadMoreNews->getResult()):
-                            foreach ($ReadMoreNews->getResult() as $n):
-                                $n['titulo'] = Check::Words($n['titulo'], 16);
-                                $n['noticia'] = Check::Words(strip_tags($n['noticia']), 36);
-                                $n['data'] = date('d/m/Y H:i', strtotime($n['data']));
-                                ?>
-                                <div class="box_morenews">
-                                    <a href="<?= HOME . '/noticia/' . $n['url_name']; ?>">
-                                        <div class="morenews_img"><img alt="<?= $n['titulo']; ?>" title="<?= $n['titulo']; ?>" src="<?= HOME . '/tim.php?src=uploads/' . $n['foto'] . '&w=380&h=180'; ?>"/></div>
-                                        <div class="morenews_dados">
-                                            <div class="morenews_datetime"><i class="fa fa-calendar"></i> <?= $n['data']; ?> hrs</div>
-                                            <div class="morenews_title"><?= $n['titulo']; ?></div>
-                                        </div>
-                                    </a>
-                                </div>
-                                <?php
+                        <div class="article_news_moreimg">
+                            <?php
+                            foreach ($OutrasFotos->getResult() as $fotos):
+                                echo '<div class="boxfotos">';
+                                echo '<img alt="' . $titulo . '" title="' . $titulo . '" src="' . HOME . '/tim.php?src=uploads/' . $fotos['foto'] . '&w=870"/>';
+                                echo '<p>' . $fotos['legenda'] . '</p>';
+                                echo '</div>';
                             endforeach;
-                        endif;
-                        ?>
+                            ?>
+                        </div>
+                        <?php
+                    endif;
+                    ?>
+                    <div class="article_news_morenews">
+                        <header class="header_morenews">
+                            <h2 class="header_morenews_vin">Outros</h2>
+                        </header>
+                        <div class="content_morenews">
+                            <?php
+                            $ReadMoreNews = new Read;
+                            $ReadMoreNews->ExeRead('noticias', "WHERE titulo != :titulo AND categoria = :categoria AND id != :id AND foto != :foto ORDER BY id DESC LIMIT :limit", "titulo=''&categoria={$categoria}&id={$id}&foto=''&limit=4");
+                            if ($ReadMoreNews->getResult()):
+                                foreach ($ReadMoreNews->getResult() as $n):
+                                    $n['titulo'] = Check::Words($n['titulo'], 16);
+                                    $n['noticia'] = Check::Words(strip_tags($n['noticia']), 36);
+                                    $n['data'] = date('d/m/Y H:i', strtotime($n['data']));
+                                    ?>
+                                    <div class="box_morenews">
+                                        <a href="<?= HOME . '/noticia/' . $n['url_name']; ?>">
+                                            <div class="morenews_img"><img alt="<?= $n['titulo']; ?>" title="<?= $n['titulo']; ?>" src="<?= HOME . '/tim.php?src=uploads/' . $n['foto'] . '&w=380&h=180'; ?>"/></div>
+                                            <div class="morenews_dados">
+                                                <div class="morenews_datetime"><i class="fa fa-calendar"></i> <?= $n['data']; ?> hrs</div>
+                                                <div class="morenews_title"><?= $n['titulo']; ?></div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <?php
+                                endforeach;
+                            endif;
+                            ?>
+                        </div>
                     </div>
-                </div>
-            </article>
+                </article>
+            </div>
         </div>
     </div>
 </div>
+
+
+
